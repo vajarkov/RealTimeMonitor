44d83d1cb21e26c109ad49a6d0d69f9d2920e55e
vajarkov <vajarkov@mail.ru>
2020-02-29 04:52:24 +0600

    Создал новый проект на C++ с CLR и написал header-файл

---
+++ RTKLibBridge/RTKLibBridge.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+#include "RTKLibBridge.h"
+
+
+using namespace RTKLibBridge;
+
+	RTKLibBridge::RTKLib::RTKLib(void) {
+
+	}
+
+
+	RTKLib::~RTKLib(void) {
+
+	}
+
+
+	RTKLib::StartServerThread(void) {
+		Init();
+	}
+
+
